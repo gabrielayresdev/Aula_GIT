1a3769eb5267317e77f2ee1001927042132f81ec
gabrielayresdev <gabriel.ar.fort@gmail.com>
2025-01-14 10:01:56 -0300

    initial commit

---
+++ file.ts
@@ -0,0 +1,26 @@
+const x = "Gabriel"; 
+const sum = 3;
+
+function Hello() {
+    return "Hello World";
+}
+
+function hi() {
+    return "Hi"
+}
+
+const authorized = true;
+const notAuthorized = true;
+
+
+if(!authorized) {
+    // faço alguma coisa
+}
+
+if(!notAuthorized) {
+    // faço outra coisa
+}
+
+function      create()        {
+    const x = 2 + 2; const y=3; return x+y;
+}
